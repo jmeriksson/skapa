e4e5f5f0a27cf0c4b9ddd2df0acd3670a4239b89
Mikael Eriksson <mikael.eriksson.nkpg@gmail.com>
2024-01-10 13:54:39 +0100

    new screenshot and favicon

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/favicon/favicon-16x16.png">
     <link rel="manifest" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/favicon/site.webmanifest">
     <link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri() ?>/assets/images/favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileColor" content="#00aba9">
     <meta name="theme-color" content="#ffffff">
 
     <?php wp_head(); ?>
