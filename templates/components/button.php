fce105e45e5af3e7d790ab6cad7f6aa44340c72d
Mikael Eriksson <mikael.eriksson.nkpg@gmail.com>
2024-03-09 19:32:23 +0100

    adding secondary button

--- templates/components/button.php
+++ templates/components/button.php
@@ -17,7 +17,11 @@ $default_args = [
 $args = wp_parse_args($args, $default_args);
 $button_classes = ['py-2 px-8 rounded font-sans no-underline hover:no-underline transition text-base md:text-lg w-fit uppercase'];
 $button_classes = array_merge($button_classes, $args['classes']);
-if ($args['color'] === 'secondary') {
+if ($args['outline'] && $args['color'] === 'primary') {
+    $button_classes[] = 'border border-primary text-primary hover:bg-primary-light hover:text-secondary';
+} else if ($args['outline'] && $args['color'] === 'secondary') {
+    $button_classes[] = 'border border-secondary text-secondary hover:bg-secondary-dark hover:text-primary';
+} else if ($args['color'] === 'secondary') {
     $button_classes[] = 'bg-secondary hover:bg-secondary-dark text-primary';
 } else {
     $button_classes[] = 'bg-primary hover:bg-primary-light text-secondary';
--- templates/modules/hero.php
+++ templates/modules/hero.php
@@ -13,13 +13,17 @@ $overlay = $args['overlay'] ?? null;
 $height = $args['height'] ?? 'large';
 $heading = $args['heading'] ?? null;
 $subheading = $args['subheading'] ?? null;
-$button = $args['button'] ?? null;
+$button_primary = $args['button_primary'] ?? null;
+$button_secondary = $args['button_secondary'] ?? null;
 
 $background_classes = [''];
 $content_classes = [''];
 $overlay_classes = [''];
 $button_color = 'primary';
 $aspect_ratio_classes = ['aspect-[3/4] sm:aspect-[1/1]'];
+$button_primary_color = 'primary';
+$button_secondary_color = 'secondary';
+
 switch ($height) {
     case 'small':
         $aspect_ratio_classes[] = 'md:aspect-[21/9]';
@@ -35,10 +39,12 @@ if ($background_type === 'color' && $background_color) {
     $content_classes = array_merge($content_classes, $aspect_ratio_classes);
     switch ($background_color) {
         case 'primary':
-            $button_color = 'secondary';
+            $button_primary_color = 'secondary';
+            $button_secondary_color = 'secondary';
             break;
         default:
-            $button_color = 'primary';
+            $button_primary_color = 'primary';
+            $button_secondary_color = 'primary';
             break;
     }
 }
@@ -49,9 +55,13 @@ if ($background_type === 'image' && $background_image) {
     switch ($text_color) {
         case 'primary':
             $background_classes[] = 'text-primary';
+            $button_primary_color = 'primary';
+            $button_secondary_color = 'primary';
             break;
         default:
-        $background_classes[] = 'text-secondary';
+            $background_classes[] = 'text-secondary';
+            $button_primary_color = 'secondary';
+            $button_secondary_color = 'secondary';
             break;
     }
 }
@@ -87,30 +97,50 @@ if ($overlay) {
         <?php endif; ?>
         <div class='<?php skapa_array_to_css_classes($content_classes); ?>'>
             <div class='container'>
-                <div class='flex flex-col items-center mx-auto max-w-5xl'>
+                <div class='flex flex-col items-center mx-auto max-w-5xl gap-4'>
                     <?php if ($heading) : ?>
                         <h1 class='h1 text-center'><?php echo esc_html($heading); ?></h1>
                     <?php endif; ?>
     
                     <?php if ($subheading) : ?>
-                        <p class='text-xl lg:text-3xl text-center uppercase'><?php echo esc_html($subheading); ?></p>
+                        <p class='text-xl lg:text-3xl text-center'><?php echo esc_html($subheading); ?></p>
                     <?php endif; ?>
     
-                    <?php
-                    if ($button) {
-                        get_template_part(
-                            'templates/components/button',
-                            null,
-                            [
-                                'title' => $button['title'],
-                                'url' => $button['url'],
-                                'color' => $button_color,
-                                'target' => $button['target'],
-                                'classes' => ['mt-8']
-                            ]
-                        );
-                    }
-                    ?>
+                    <?php if ($button_primary || $button_secondary) : ?>
+                        <div class="flex flex-col sm:flex-row gap-4 sm:gap-8 mt-4">
+                            <?php
+                            if ($button_primary) {
+                                get_template_part(
+                                    'templates/components/button',
+                                    null,
+                                    [
+                                        'title' => $button_primary['title'],
+                                        'url' => $button_primary['url'],
+                                        'color' => $button_primary_color,
+                                        'target' => $button_primary['target'],
+                                        'classes' => ['w-full sm:w-auto']
+                                    ]
+                                );
+                            }
+                            ?>
+                            <?php
+                            if ($button_secondary) {
+                                get_template_part(
+                                    'templates/components/button',
+                                    null,
+                                    [
+                                        'title' => $button_secondary['title'],
+                                        'url' => $button_secondary['url'],
+                                        'color' => $button_secondary_color,
+                                        'target' => $button_secondary['target'],
+                                        'outline' => true,
+                                        'classes' => ['w-full sm:w-auto']
+                                    ]
+                                );
+                            }
+                            ?>
+                        </div>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
